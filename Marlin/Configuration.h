ac537dc6df08fdf56432094b4b3ad7d5b90bf944
Matthias Hryniszak <mhryn@softserveinc.com>
2020-08-22 16:03:50 +0200

    Increased mesh inset size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1130,10 +1130,10 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 20
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS (Y_BED_SIZE-20)
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 
 /**
@@ -1316,7 +1316,7 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 25             // Set Mesh bounds as an inset region of the bed
+  #define MESH_INSET 35             // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
